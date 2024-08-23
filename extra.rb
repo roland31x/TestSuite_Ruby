dabd9cfa10ea842171ed7801fd930263ba4dc97d
roland31x <roland31x@gmail.com>
2024-08-23 19:05:00 +0000

    Add some extra functions

--- extra.rb
+++ extra.rb
@@ -1,48 +1,54 @@
 module Extra
 
-  def self.random_string(length=10)
-    chars = [*'A'..'Z', *'a'..'z', *'0'..'9']
-    (0...length).map { chars.sample }.join
+  def self.is_prime?(num)
+    return false if num < 2
+    (2..Math.sqrt(num)).each { |i| return false if (num % i).zero? }
+    true
   end
 
-  def self.factorial(n)
-    return 1 if n == 0
-    n * factorial(n - 1)
+  def self.gcd(a, b)
+    b == 0 ? a : gcd(b, a % b)
   end
 
-  def self.is_palindrome?(str)
-    s = str.downcase.gsub(/\s+/, "")
-    s == s.reverse
+  def self.lcm(a, b)
+    (a * b) / gcd(a, b)
   end
 
-  def self.unique_elements(arr)
-    arr.uniq
+  def self.flatten_hash(hash, prefix = nil)
+    hash.each_with_object({}) do |(k, v), h|
+      key = prefix ? "#{prefix}.#{k}" : k.to_s
+      if v.is_a?(Hash)
+        h.merge!(flatten_hash(v, key))
+      else
+        h[key] = v
+      end
+    end
   end
 
-  def self.flatten_list(nested_arr)
-    nested_arr.flatten
+  def self.most_common(arr)
+    arr.group_by(&:itself).max_by { |_, v| v.size }&.first
   end
 
-  def self.fibonacci(n)
-    a, b = 0, 1
-    n.times { a, b = b, a + b }
-    a
+  def self.transpose(matrix)
+    matrix[0].zip(*matrix[1..-1])
   end
 
-  def self.count_vowels(str)
-    str.count("aeiouAEIOU")
+  def self.camel_to_snake(str)
+    str.gsub(/([A-Z])/, '_\1').downcase.sub(/^_/, '')
   end
 
-  def self.merge_hashes(*hashes)
-    hashes.reduce({}, :merge)
+  def self.swap_case(str)
+    str.chars.map { |c| c =~ /[A-Z]/ ? c.downcase : c.upcase }.join
   end
 
-  def self.reverse_words(sentence)
-    sentence.split.map(&:reverse).join(" ")
+  def self.nested_sum(arr)
+    arr.reduce(0) do |sum, el|
+      sum + (el.is_a?(Array) ? nested_sum(el) : el.to_i)
+    end
   end
 
-  def self.chunk_list(arr, chunk_size)
-    arr.each_slice(chunk_size).to_a
+  def self.digit_sum(num)
+    num.to_s.chars.map(&:to_i).reduce(:+)
   end
 
 end
